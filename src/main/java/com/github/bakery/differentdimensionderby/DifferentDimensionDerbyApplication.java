f7a1b092f0a092c19f2eec1307bf280e71647ab0
hotire <gngh0101@gmail.com>
2021-12-06 23:41:56 +0900

    init

---
+++ src/main/java/com/github/bakery/differentdimensionderby/DifferentDimensionDerbyApplication.java
@@ -0,0 +1,13 @@
+package com.github.bakery.differentdimensionderby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferentDimensionDerbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DifferentDimensionDerbyApplication.class, args);
+    }
+
+}
